99ae3bbe268254888c1ec15db8474524b80e3a86
ST10384480 <131756565+ST10384480@users.noreply.github.com>
2025-03-18 22:10:20 +0200

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ST10384480_CLDV_Part1.Models;
+
+namespace ST10384480_CLDV_Part1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
